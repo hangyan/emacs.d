075cb077c6bc7395f66a17ea2a805c5dbd8ac0a5
Hang Yan <yhang@vmware.com>
2021-06-17 17:30:56 +0800

    Add protobuf package

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -2,9 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
+;; how to install godef: go get github.com/rogpeppe/godef
+
 (require-package 'go-mode)
 (autoload 'go-mode "go-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+
+
+
+;; protobuf
+(require-package 'protobuf-mode)
 
 (provide 'init-golang)
 ;;; init-rust.el ends here
